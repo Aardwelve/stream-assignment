d08a090e8ba5938a193e272421e7fca15e637929
jamerrrson <102266472+jamerrrson@users.noreply.github.com>
2022-04-28 11:32:00 +1000

    Add files via upload

---
+++ enhancements.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<meta name="description" content="Enhancement Page" />
+	<meta name="keywords" 	 content="HTML, CSS" />
+	<meta name="author" 	 content="Stream"/>
+	<title>Enhancement</title>
+	<link href = "styles/style.css" rel="stylesheet">
+</head>
+
+<body>
+<?php
+	include('header.inc');
+?>
+<?php
+	include('menu.inc');
+?>
+<hr>
+<h2>Enhancement 1</h2>
+<h3>Embedded Video Content</h3>
+<p>The embedded video is used as a tutorial / guide of the website for other users to see the functionality of the page. For eg. <a href="index.html">index</a></p>
+
+<h2>Enhancement 2</h2>
+<h3>Vertical Line</h3>
+<p>The usage of CSS allowed for the implementation of a verticle line to seperate the page on the topic <a href="topic.html">page</a> but easier to see on the topic page itself. It allows for seperation between the aside section and the main paragraphs</p>
+<?php
+	require('footer.inc');
+?>
+</body>
+</html>
