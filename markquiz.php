6bf6a35a3dfa86f8c8050224744b965ff20e5dd3
sirjesse <75675945+sirjesse@users.noreply.github.com>
2022-05-19 21:10:13 +1000

    Fixed markquiz.php
    Now correctly adds data to SQL database and will check the number of attempts is not greater than 2 based on Student ID.

--- markquiz.php
+++ markquiz.php
@@ -1,21 +1,13 @@
 <!DOCTYPE html>
-<!--code by webdevtrick (webdevtrick.com) -->
 <html>
- 
-<head>
- <meta charset=UTF-8" />
- 
- <title>PHP Quiz</title>
- 
- <link href = "styles/style.css" rel="stylesheet">
-</head>
- 
-<body>
- 
- <div id="page-wrap">
- 
- <h1>Multiple Choice Results</h1>
- 
+	<head>
+ 	<meta charset="UTF-8" />
+	<title>PHP Quiz</title>
+	<link href = "styles/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<div id="page-wrap">
+ 		<h1>Multiple Choice Results</h1>
         <?php
 			require_once ("settings.php");
 			$conn = @mysqli_connect($host,
@@ -28,103 +20,43 @@
 			} else {
 				date_default_timezone_set("Australia/Melbourne");
 
-				$fname = $_POST['fname'];
-				$studentid = $_POST['StudentID'];
+				$sql_table = "attempts";
+
+				$fname = trim($_POST['fname']);
+				$studentid = trim($_POST['StudentID']);
 				$date = date('Y/m/d H:i:s');
-				$attempt = 0;
+				$attempt = "SELECT * FROM attempts WHERE Student_ID = $studentid";
 
-				$answer1 = $_POST['radioq'];
-				$answer2 = $_POST['checkq'];
-				$answer3 = $_POST['drop_down'];
-				$answer4 = $_POST['year'];
-				
+				$attemptcheck = mysqli_query($conn, $attempt);
+				if($attemptcheck->num_rows >= 2) {
+					echo "<p>Error: You have surpassed the maximum number of allowed attempts</p>";
+				} else {
+					$answer1 = $_POST['radioq'];
+					$answer2 = $_POST['check1'];
+					$answer3 = $_POST['drop_down'];
+					$answer4 = $_POST['year'];
+					
 
-				$totalCorrect = 0;
-				
-				if ($answer1 == "Answer4") { $totalCorrect++; }
-				if ($answer2 == "Answer1" && "Answer4") { $totalCorrect++; }
-				if ($answer3 == "BTV") { $totalCorrect++; }
-				if ($answer4 == "1993") { $totalCorrect++; }
+					$totalCorrect = 0;
+					
+					if ($answer1 == "Answer4") { $totalCorrect++; }
+					if ($answer2 == "Answer1" && "Answer4") { $totalCorrect++; }
+					if ($answer3 == "BTV") { $totalCorrect++; }
+					if ($answer4 == "1993") { $totalCorrect++; }
 
 
-				$query = "insert into $sql_table (Date & Time, First Name & Last Name, Student ID, No. Attempt, Score) values ('$date', '$fname', '$studentid', '$attempt', '$totalCorrect')";	
-			
-			
-				echo "<div id='results'>$totalCorrect / 4 correct</div>";
+					$query = "INSERT INTO $sql_table (Attempt_ID, Date_Time, FirstName_LastName, Student_ID, No_Attempt, Score) VALUES (NULL, '$date', '$fname', '$studentid', '$attempt', '$totalCorrect')";
+
+					$result = mysqli_query($conn, $query);
+					if(!$result) {
+						echo "<p>Something is wrong with ", $query, "</p>";
+					} else {
+						echo "<p class=\"ok\"><div id='results'>$totalCorrect / 4 correct</div></p>";
+					}
+					mysqli_close($conn);
+				}
 			}
         ?>
-					
-					<?php
-		if (isset ($_POST["fname"])){
-			$fname = $_POST["fname"];
-			$fname = sanitise_input($fname);
-		if (isset ($_POST["StudentID"]));
-			$StudentID = $_POST["StudentID"];
-			$StudentID = sanitise_input($StudentID);
-		if (isset ($_POST["Date"]));
-			$Date = $_POST["Date"];
-			$Date = sanitise_input($Date);
-		}
-		else {
-			header ("location: register.html"); 
-		}
-			
-			function sanitise_input($data) {
-				$data = trim($data);
-				$data = stripslashes($data);
-				$data = htmlspecialchars($data);
-				return $data;
-			}
-			
-				$errMsg = " ";
-			if ($fname==" ") {
-				$errMsg .= "<p>You must enter an age.</p>";
-			}
-			else if (!preg_match("/^[a-zA-Z ][0,30]+$/D", $fname)) {
-				$errMsg .= "<p>Only 30 max Alpha letters and hyphens allowed in your last name.</p>";
-			}
-			if ($errMsg != " "){
-				echo "<p>$errMsg</p>";
-			}
-			else {
-				
-			
-			$errMsg = " ";
-			if ($StudentID==" ") {
-				$errMsg .= "<p>You must enter an age.</p>";
-			}
-			else if (!preg_match("/^[7,10]*$", $StudentID)) {
-				$errMsg .= "<p>only 7 or 10 digits allowed</p>";
-			}
-			if ($errMsg != " "){
-				echo "<p>$errMsg</p>";
-			}
-			else {
-					echo "<table width="200" border="1">\n";
-					echo "<tbody>\n
-					<tr>\n
-					<th scope="Field">&nbsp;</th>\n
-					<th scope="Format Requirement">&nbsp;</th>\n
-					</tr>\n
-					<tr>\n
-					<td>Student ID</td>\n
-					<td>$StudentID</td>\n
-					</tr>\n
-					<tr>\n
-					<td>First & Last Name</td>\n
-					<td>$fname</td>\n
-					</tr>\n
-					</tbody>\n
-					</table>\n";
-
-			}
-			}
-			
-?>
-
- 
- </div>
- 
-</body>
- 
+ 		</div>
+	</body>
 </html>
