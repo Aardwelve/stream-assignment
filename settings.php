028522b556c0b71e5a6a4942f43263e35950869e
sirjesse <75675945+sirjesse@users.noreply.github.com>
2022-05-26 11:09:32 +1000

    update settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
-    $host = "";
-    $user = "";
-    $pwd = "";
-    $sql_db = "";
+    $host = "feenix-mariadb.swin.edu.au";
+    $user = "s102725583";
+    $pwd = "281100";
+    $sql_db = "s102725583_db";
 ?>
