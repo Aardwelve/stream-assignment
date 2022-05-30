ddaf9ee3bef17d3981a4e8336f7a1a7eab248468
NutetuN <68525151+NutetuN@users.noreply.github.com>
2022-05-30 16:58:45 +1000

    Update index.php

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
     include('menu.inc');
 ?>
 	
-	<a id="vidtext" href="https://www.youtube.com/watch?v=oBA_FdQZsYc"><h2>Streaming Media Video</h2></a>
-	<iframe id="video" src="https://www.youtube.com/embed/oBA_FdQZsYc" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<a id="vidtext" href="https://video.alexanderok.com"><h2>Streaming Media Video</h2></a>
+	<iframe id="video" src="https://video.alexanderok.com" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	
 <hr>
 <?php
